dbdbcf44eb8ac87b7dadea7e9791beb41c2ee3cd
Greg Palmer <greg@gmp.dev>
2023-08-10 17:46:19 -0700

    Save teamslist and teams data to GCS bucket

--- src/lib/fetch-teams.ts
+++ src/lib/fetch-teams.ts
@@ -1,3 +1,6 @@
+import 'dotenv/config';
+import { Storage } from '@google-cloud/storage';
+
 import { delayedIterator } from '../utils/index.js';
 import { fetchTeamList, fetchTeam } from '../api/client.js';
 
@@ -6,15 +9,35 @@ function saveData(data: any): void {
 }
 
 export async function fetchTeams() {
+  const jobId = 'fetch-teams';
+  const time = new Date();
+  const storage = new Storage({
+    projectId: process.env.GCP_PROJECT_ID,
+  });
+  const bucket = storage.bucket(
+    process.env.GCP_DATA_EXTRACT_BUCKET_NAME as string,
+  );
+
   const teamsList = await fetchTeamList();
   if (!teamsList) {
     throw new Error('Failed to fetch teamsList');
   }
 
-  saveData(teamsList);
+  await bucket
+    .file(`${jobId}/${time.toISOString()}/teams-list.json`)
+    .save(JSON.stringify(teamsList));
+  console.log(`SAVED = ${jobId}/${time.toISOString()}/teams-list.json`);
 
   for await (const { $ref } of delayedIterator(teamsList.items, 300)) {
-    const teamData = await fetchTeam({ $ref });
-    saveData(teamData);
+    const team = await fetchTeam({ $ref });
+    if (!team) {
+      throw new Error(`Failed to fetch team ${$ref}`);
+    }
+    await bucket
+      .file(`${jobId}/${time.toISOString()}/${team.id}-${team.slug}.json`)
+      .save(JSON.stringify(team));
+    console.log(
+      `SAVED = ${jobId}/${time.toISOString()}/${team.id}-${team.slug}.json`,
+    );
   }
 }
