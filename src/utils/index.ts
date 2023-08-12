0ba6e6daccf455dd3c3cbd11d76385e0c713e1ac
Greg Palmer <greg@gmp.dev>
2023-08-12 10:57:52 -0700

    Remove local filepath storage utils

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,6 +1,3 @@
-import { fileURLToPath } from 'url';
-import path from 'path';
-
 export function delay(ms: number) {
   return new Promise((resolve) => setTimeout(resolve, ms));
 }
@@ -10,16 +7,4 @@ export async function* delayedIterator(array: any[], ms: number) {
     yield item;
     await delay(ms);
   }
-}
-
-export function getOutputDataRootDir() {
-  const __filname = fileURLToPath(import.meta.url);
-  const __dirname = path.dirname(__filname);
-  return path.resolve(__dirname, '../../data');
-}
-
-export function getOutputDataDirForType(type: string) {
-  const date = new Date();
-  const root = getOutputDataRootDir();
-  return `${root}/${type}/${date.toISOString()}`;
 }
